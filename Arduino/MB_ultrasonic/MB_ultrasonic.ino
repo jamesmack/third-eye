345be5cbb6767e92569edd862b8168421c8caeeb
James Mackenzie <jamesmack1@gmail.com>
2015-02-19 18:52:18 -0800

    Added QueueList and averaging
    After four successive sensor readings (where the readings are pushed
    into a QueueList), the values are averaged using a power of two
    averaging method.

--- Arduino/MB_ultrasonic/MB_ultrasonic.ino
+++ Arduino/MB_ultrasonic/MB_ultrasonic.ino
@@ -1,20 +1,38 @@
 #include <SimpleTimer.h>
+#include <QueueList.h>
 
 SimpleTimer timer;
-const int pwPin1 = 9;
-long pulse1, sensor1;
+const int sensor_pin = 9;
+long distance, reading;
+QueueList <int> sensor_readings;
+const int avg_power = 2;     // 1 for 2 readings, 2 for 4 readings, 3 for 8, etc.
+const int avg_count = pow(2, avg_power);
+
+int average(QueueList<int> & q, int avg_power) {
+    long sum = 0;
+    for (int i = 0; i < avg_count; i++) {
+        sum += q.pop();
+    }
+    return (sum >> avg_power);
+}
+
+void readSensor() {
+  reading = pulseIn(sensor_pin, HIGH);
+  distance = reading/147;
+  Serial.print(distance);
+  Serial.println(" ");
+  
+  sensor_readings.push(distance);
+  if(sensor_readings.count() > avg_count) {
+        int avg = average(sensor_readings, avg_power);
+        Serial.println(avg);
+  }
+}
 
 void setup () {
   Serial.begin(9600);
-  pinMode(pwPin1, INPUT);
-  timer.setInterval(100, read_sensor);
-}
-
-void read_sensor(){
-  pulse1 = pulseIn(pwPin1, HIGH);
-  sensor1 = pulse1/147;
-  Serial.print(sensor1);
-  Serial.println(" ");
+  pinMode(sensor_pin, INPUT);
+  timer.setInterval(75, readSensor);
 }
 
 void loop() {
